6d4ef1b17501abd3f2e476fc450bd30f54a0145d
retxxxirt <retxxxirt@gmail.com>
2018-04-17 19:16:51 +0300

    add requests[socks] to requirements

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
 
     name = 'proxy_list',
     packages = ['proxy_list'],
-    version = '0.3.4',
+    version = '0.3.5',
     description = 'Free proxy lists',
     author = 'retxxxirt',
     author_email = 'retxxirt@gmail.com',
     url = 'https://github.com/retxxxirt/proxy_list',
     keywords = ['proxy', 'proxies', 'proxy list', 'free', 'free proxy', 'free proxies'],
-    install_requires = ['requests', 'pyquery', 'iso3166', 'js2py'],
+    install_requires = ['requests[socks]', 'pyquery', 'iso3166', 'js2py'],
     package_data = {'proxy_list': ['parsers/*.py']}
 )
